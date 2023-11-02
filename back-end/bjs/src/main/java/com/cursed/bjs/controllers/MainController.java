ccd8e88cabc81e5616332cebe705215fd1e86198
CursedBirb <AmrodV2@gmail.com>
2023-11-02 13:42:42 +0100

    Mam chwilowo dosc tego

--- back-end/bjs/src/main/java/com/cursed/bjs/controllers/MainController.java
+++ back-end/bjs/src/main/java/com/cursed/bjs/controllers/MainController.java
@@ -4,15 +4,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.ui.Model;
 
-@CrossOrigin(origins = "http://localhost:8001")
+@CrossOrigin(origins = "http://localhost:3000")
 @Controller
 public class MainController {
 
-        @RequestMapping("/powitanie")
+    @RequestMapping("/powitanie")
     public String greeting(@RequestParam(value="myname", required=false, defaultValue="nieznany") String myname, Model model) {
         
         model.addAttribute("name", myname); //Dodanie atrybutu do lokalnej pamieci tzw. modelu
@@ -20,7 +22,8 @@ public class MainController {
         return "powitanie"; //Przekierowanie do strony powitanie.html
     }
 
-    @GetMapping("/api/hello")
+    @RequestMapping(value = "/api/hello", method = RequestMethod.GET)
+    @ResponseBody
     public String sayHello() {
         return "Hello from Spring Boot!";
     }
