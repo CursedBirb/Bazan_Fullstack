04965ad84a01ab44de91a41d87b36026830e58f5
CursedBirb <AmrodV2@gmail.com>
2023-11-02 11:45:47 +0100

    Initial creation

---
+++ back-end/bjs/src/test/java/com/cursed/bjs/BjsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursed.bjs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BjsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
