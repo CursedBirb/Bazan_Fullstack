7b5705221725e6ae8d259c3a284775b90ff9dfb6
CursedBirb <AmrodV2@gmail.com>
2023-11-02 12:25:42 +0100

    Of course it would happen

---
+++ back-end/bjs/src/main/java/com/cursed/bjs/BjsApplication.java
@@ -0,0 +1,13 @@
+package com.cursed.bjs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BjsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BjsApplication.class, args);
+	}
+
+}
