a8efa3c03e457128c5416d2d0c4f82246795a8b3
CursedBirb <AmrodV2@gmail.com>
2023-11-23 15:25:16 +0100

    Database works. Now figure how to make querry sending by api works

--- back-end/bjs/src/main/java/com/cursed/bjs/database/HiraganaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cursed.bjs.database;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HiraganaRepository extends JpaRepository<Hiragana, Integer> {
-    // Dodaj dodatkowe metody zapytań, jeśli są potrzebne
-}
