5693baa11dc64b34bcc28c4ba6b345d22837c6ec
Cursed <47064429+CursedBirb@users.noreply.github.com>
2024-02-28 14:07:53 +0100

    Update backendAdress.js

--- frontend/src/components/backendAdress.js
+++ frontend/src/components/backendAdress.js
@@ -1,5 +1,5 @@
-//const backendAdress = 'http://localhost:8080';
+const backendAdress = 'http://localhost:8080';
 //const backendAdress = 'process.env.REACT_APP_BACKEND_ADDRESS' || 'http://localhost:8080';
-const backendAdress = 'http://backend-spring:8080';
+//const backendAdress = 'http://backend-spring:8080';
 
 export default backendAdress;
